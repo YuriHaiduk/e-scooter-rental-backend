e07c6f9f1493f36c2d1199dd8a9f3b1e37f6dcb2
Yuri Haiduk <yurihaiduk@gmail.com>
2023-08-15 17:06:45 +0300

    hotfix

--- app/Services/DrivingPriceService.php
+++ app/Services/DrivingPriceService.php
@@ -5,16 +5,17 @@ namespace App\Services;
 use App\Models\Type;
 use Carbon\Carbon;
 
-class DrivingPriceService {
+class DrivingPriceService
+{
 
-    public static function calculatePrice(int $type_id, string $startTime, string $stopTime = null): int
+    public static function calculatePrice(int $typeId, string $startTime, string $stopTime = null): int
     {
         $start = new Carbon($startTime);
         $stop = (!is_null($stopTime)) ? new Carbon($stopTime) : now();
 
         $totalTimeByMinutes = $stop->diffInMinutes($start);
 
-        $priceByMinutes = Type::find($type_id)->price_per_hour / 60;
+        $priceByMinutes = Type::find($typeId)->price_per_hour / 60;
 
         return ceil($totalTimeByMinutes * $priceByMinutes);
     }
