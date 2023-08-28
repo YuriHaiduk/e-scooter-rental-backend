a40c20ad26bea02c11a4d391277fcb7574020834
Yuri Haiduk <yurihaiduk@gmail.com>
2023-08-28 21:27:38 +0300

    hotfix

--- app/Repositories/DrivingRepository.php
+++ app/Repositories/DrivingRepository.php
@@ -16,12 +16,16 @@ class DrivingRepository
 
     public function getActiveDrivings()
     {
-        return Driving::active()->latest('start_time')->paginate();
+        return Driving::active()
+            ->latest('start_time')
+            ->paginate();
     }
 
     public function isScooterAlreadyActive(int $scooterId): bool
     {
-        return Driving::active()->where('scooter_id', $scooterId)->exists();
+        return Driving::active()
+            ->where('scooter_id', $scooterId)
+            ->exists();
     }
-    
+
 }
